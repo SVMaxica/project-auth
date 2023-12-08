656f4390c5a3872f1f2a9d955425b60b04dbfcbc
Sandra Gustafsson <sandragustafsson@Mac-mini.local>
2023-12-08 10:59:22 +0100

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build", // Specify the output directory
+    assetsDir: "", // Assets directory (relative to outDir)
+  },
 })
